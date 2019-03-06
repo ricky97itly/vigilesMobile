db85781de51069a10c4369c8f48adec2d2eacbaa
lolliwow <claudia.lolli97@gmail.com>
2019-03-06 15:41:37 +0100

    Claudia
    Claudia quella vera

--- vigilesMobile/ViewController.swift
+++ vigilesMobile/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("ciao come stai?")
         print("hello, S.")
+        print("Claudia bella")
     }
 
 
