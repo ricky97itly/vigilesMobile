58c969c057475a2f8a6852d64816fdacbb35602c
SaraCarnevali <sara.carnevali@ied.edu>
2019-03-06 14:24:19 +0100

    Commit Sara
    Commit di prova

---
+++ vigilesMobile/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  vigilesMobile
+//
+//  Created by Riccardo Mores on 06/03/2019.
+//  Copyright © 2019 Riccardo Mores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("ciao come stai?")
+        print("hello, S.")
+    }
+
+
+}
+
---
+++ vigilesMobileTests/vigilesMobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  vigilesMobileTests.swift
+//  vigilesMobileTests
+//
+//  Created by Riccardo Mores on 06/03/2019.
+//  Copyright © 2019 Riccardo Mores. All rights reserved.
+//
+
+import XCTest
+@testable import vigilesMobile
+
+class vigilesMobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
