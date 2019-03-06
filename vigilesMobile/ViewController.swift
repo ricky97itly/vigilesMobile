741b9e027b967a4ea71f8f5915666e71b521b26d
SaraCarnevali <sara.carnevali@ied.edu>
2019-03-06 15:46:30 +0100

    Sara
    Saraaaa

--- vigilesMobile/ViewController.swift
+++ vigilesMobile/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("ciao come stai?")
-        print("hello, S.")
+        print("hello, S.ara")
         print("Claudia bella")
     }
 
