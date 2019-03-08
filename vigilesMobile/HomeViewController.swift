8dd96e9dee6d7fec3b39df2d8de15f8ea4b64f89
Riccardo Mores <riccardo.mores@ied.edu>
2019-03-08 19:25:30 +0100

    TabBar
    Creazione TabBar + ordinamento

---
+++ vigilesMobile/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  vigilesMobile
+//
+//  Created by Riccardo Mores on 08/03/2019.
+//  Copyright © 2019 Riccardo Mores. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+     @IBOutlet var homeSwipe: UISwipeGestureRecognizer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    @IBAction func homeSwipe(sender:UISwipeGestureRecognizer) {
+//        if sender.direction == UISwipeGestureRecognizer.Direction.right {
+//            self.tabBarController?.selectedIndex = 2
+//        }
+//    }
+   
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
