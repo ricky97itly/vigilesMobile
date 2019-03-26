b52bada0cca0c260a0f56bea6c546321ec64b9af
lolliwow <claudia.lolli97@gmail.com>
2019-03-26 15:02:39 +0100

    Commit Cla + Ric
    CollectionView Babyyyyy (nel cerca)

---
+++ vigilesMobile/DataCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DataCollectionViewCell.swift
+//  vigilesMobile
+//
+//  Created by Claudia Lolli on 26/03/2019.
+//  Copyright © 2019 Riccardo Mores. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var codeImg: UIImageView!
+    @IBOutlet weak var emergencyTitle: UILabel!
+    @IBOutlet weak var emergencyAddress: UILabel!
+    @IBOutlet weak var emergencyDescription: UILabel!
+    
+    
+}
--- vigilesMobile/SearchViewController.swift
+++ vigilesMobile/SearchViewController.swift
@@ -11,16 +11,20 @@ import UIKit
 class SearchViewController: UIViewController, UISearchBarDelegate  {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var chatBtn: UIButton!
-   
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
+    var testImg = [UIImage(named: "infantes"), UIImage(named: "tony"), UIImage(named: "solcia")]
+    var testAddress = ["ciao", "prova", "claudia"]
+    var testDescription = ["Nel mezzo del cammin di nostra claudia", "claudia ciao come stai??", "Madonna bonina san pancrazio cicciopuzzo ace + fabri no comment..."]
+    var testTitle = ["mena", "porcatroia", "brutta zoccola"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.self.delegate = self
         self.searchBar.layer.cornerRadius = 20
         self.searchBar.clipsToBounds = true
-
+       
         // Do any additional setup after loading the view.
     }
     
@@ -54,4 +58,19 @@ class SearchViewController: UIViewController, UISearchBarDelegate  {
     }
     */
 
+}
+
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testTitle.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cella", for: indexPath) as? DataCollectionViewCell
+        cell?.emergencyTitle.text = testTitle[indexPath.row]
+        cell?.codeImg.image = testImg[indexPath.row]
+        cell?.emergencyAddress.text = testAddress[indexPath.row]
+        cell?.emergencyDescription.text = testDescription[indexPath.row]
+        return cell!
+    }
 }
