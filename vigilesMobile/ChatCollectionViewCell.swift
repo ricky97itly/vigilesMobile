09e3aaabe81015a1ebf3df59dd4a0abe23fb2a59
lolliwow <claudia.lolli97@gmail.com>
2019-03-28 15:56:52 +0100

    Chat
    Collection View chat con collegamento alla home.

---
+++ vigilesMobile/ChatCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatCollectionViewCell.swift
+//  vigilesMobile
+//
+//  Created by Claudia Lolli on 28/03/2019.
+//  Copyright © 2019 Riccardo Mores. All rights reserved.
+//
+
+import UIKit
+
+class ChatCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ vigilesMobile/ChatViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChatViewController.swift
+//  vigilesMobile
+//
+//  Created by Claudia Lolli on 28/03/2019.
+//  Copyright © 2019 Riccardo Mores. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var chatCV: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
