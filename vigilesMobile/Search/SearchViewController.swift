ee4c49d56c552c65bfec814f6815b99d9720747a
Riccardo Mores <riccardo.mores@ied.edu>
2019-05-03 10:36:46 +0200

    Cerca finito
    Popolata TB del cerca con dati dal DB, la ricerca funziona per indirizzo

--- vigilesMobile/Search/SearchViewController.swift
+++ vigilesMobile/Search/SearchViewController.swift
@@ -7,33 +7,36 @@
 //
 
 import UIKit
+import Kingfisher
 
-var titleSearch = ["boh"]
-var addressSearch = ["Prova"]
-var descriptionSearch = ["funziona coglione della merda"]
-
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var searchTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
-
-    var testImg = [UIImage(named: "infantes"), UIImage(named: "tony"), UIImage(named: "solcia")]
-    var testAddress = ["ciao", "prova", "claudia"]
-    var testDescription = ["Nel mezzo del cammin di nostra claudia", "claudia ciao come stai??", "Madonna bonina san pancrazio cicciopuzzo ace + fabri no comment..."]
-    var testTitle = ["mena", "porcatroia", "brutta zoccola"]
-    var emergencyArray = [Emergency]()
-    var selectedEmergencyArray = [Emergency]()
-
-
+    var reports = [Reports]()
+    var originalReports = [Reports]()
+    var selectedReports = [Reports]()
+    var reportImgs = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.self.delegate = self
         self.searchBar.layer.cornerRadius = 20
         self.searchBar.clipsToBounds = true
-        setUpEmergency()
-       
-        // Do any additional setup after loading the view.
+        originalReports = reports
+        selectedReports = reports
+        ReportsModel().fetchEvents(complete: {
+            (reports) in self.reports = reports
+            //            Viene gestita l'esecuzione di più elementi di lavoro
+            let queue = DispatchQueue.main
+            //            Vengono eseguite più azioni "in parallelo"
+            queue.async(execute: {
+                self.searchTableView.delegate = self
+                self.searchTableView.dataSource = self
+                self.searchTableView?.reloadData()
+            })
+        })
     }
 
     
@@ -47,33 +50,23 @@ class SearchViewController: UIViewController {
         return (true)
     }
     
-//    Riempio array emergenze
-    func setUpEmergency() {
-        emergencyArray.append(Emergency(title: "Albero caduto", address: "Via Papa Roma, 32", description: "Un albero è caduto sopra una Tesla", code: SearchViewController.Emergency.CodeType(rawValue: "Verde")!))
-        emergencyArray.append(Emergency(title: "Incendio", address: "Via Scheiwiller, 1", description: "Incendio dovuto ad un cassonetto", code: SearchViewController.Emergency.CodeType(rawValue: "Giallo")!))
-        emergencyArray.append(Emergency(title: "Ascensore bloccato", address: "Via Amatore Sciesa, 4", description: "Sono rimasto bloccato nell'ascensore dello ied", code: SearchViewController.Emergency.CodeType(rawValue: "Rosso")!))
-        
-        selectedEmergencyArray = emergencyArray
-    }
-}
-
-extension SearchViewController: UITabBarDelegate, UITableViewDataSource, UISearchBarDelegate {
-    
-    func setUpSearchBar() {
-        searchBar.delegate = self
-    }
-    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("mortacci tua")
+        originalReports = reports
         guard !searchText.isEmpty
             else {
-                selectedEmergencyArray = emergencyArray
+                print("mena")
                 searchTableView.reloadData()
                 return
         }
-        selectedEmergencyArray = emergencyArray.filter({
-            emergency -> Bool in emergency.address.lowercased().contains(searchText.lowercased())
+        print("taralli")
+        originalReports = reports
+        selectedReports = reports.filter({
+            reports -> Bool in (reports.address?.lowercased().contains(searchText.lowercased()))!
         })
-        
+        print(reports)
+        print(selectedReports)
+        print("claudia hai rotto il cazzo ma tvb")
         searchTableView.reloadData()
     }
     
@@ -93,55 +86,40 @@ extension SearchViewController: UITabBarDelegate, UITableViewDataSource, UISearc
 //        searchTableView.reloadData()
 //    }
     
-    class Emergency {
-        var title: String
-        var address: String
-        var description: String
-        var code: CodeType
-        
-//        Definisco i valori da dare all'oggetto
-
-        init(title: String, address: String, description: String, code: CodeType) {
-            self.title = title
-            self.address = address
-            self.description = description
-            self.code = code
-        }
-        
-//        Raggruppamento oggetti
-        
-        enum CodeType: String {
-            case green = "Verde"
-            case yellow = "Giallo"
-            case red = "Rosso"
-        }
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return selectedEmergencyArray.count
+        //        Richiama il numero di elementi presenti nel db. Non fate il mio errore di mettere 1 o avrete una sola cella
+        return selectedReports.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cella")
         let cell = tableView.dequeueReusableCell(withIdentifier: "cella", for: indexPath) as? SearchTableViewCell
-        cell?.searchName.text = selectedEmergencyArray[indexPath.row].title
-        cell?.searchAddress.text = selectedEmergencyArray[indexPath.row].address
-        cell?.searchDescription.text = selectedEmergencyArray[indexPath.row].description
+        if self.selectedReports.count > 0 {
+            let report = self.selectedReports[indexPath.row]
+            cell?.searchName.text = report.title
+            //            cell?.emergencyId.text = report.id
+            cell?.searchAddress.text = report.address
+            //            cell?.streetNumber.text = report.street_number
+            cell?.searchDescription.text = report.description
+            //            Converto url in stringa
+            let imgUrl = URL(string: report.media!)
+            //            kf è metodo diKingFisher
+            cell?.codeImg.kf.setImage(with: imgUrl)
+        }
         cell?.layer.cornerRadius = 15
         return (cell!)
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == UITableViewCell.EditingStyle.delete {
-            testTitle.remove(at: indexPath.row)
-            searchTableView.reloadData()
-        }
-    }
+//    //    Gestisce la cancellazione di una cella
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == UITableViewCell.EditingStyle.delete {
+//            reports.remove(at: indexPath.row)
+//            searchTableView.reloadData()
+//        }
+//    }
+
     
-    override func viewDidAppear(_ animated: Bool) {
-        searchTableView.reloadData()
-    }
 }
 
 
+    
 
